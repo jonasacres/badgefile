88c989cbe41a395f9431d6ea0f20891ea2b607e8
Jonas Acres <jonas@unitymath.net>
2025-01-21 07:35:11 -0800

    remove TODO

--- src/datamgmt/issue_checks/registration_wrong_type.py
+++ src/datamgmt/issue_checks/registration_wrong_type.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 def run_check(attendee):
-  # TODO: figure out actual youth registration cutoff
   type = attendee.info()['regtype'].lower()
   dob = attendee.date_of_birth()
   youth_cutoff = datetime(2008, 7, 20)
