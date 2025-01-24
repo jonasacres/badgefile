3c06938ae202af2ae814265d7fbf0c9a2f27d5de
Jonas Acres <jonas@unitymath.net>
2025-01-23 21:51:48 -0800

    cleaned up stubbed out source files

--- src/datamgmt/issue.py
+++
@@ -1,9 +0,0 @@
-# describes an issue that needs to be resolved for an attendee to be fully checked in.
-class Issue:
-  def __init__(self, attendee, issue_code, info):
-    pass
-  
-  # based on issue_code and info, check to see if the issue is resolved
-  def check_for_resolution(self):
-    pass
-  
