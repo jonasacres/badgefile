ec639e9a131bd938c0db1a21b9385f2c60c8f98c
Jonas Acres <jonas@unitymath.net>
2025-04-26 11:20:11 -0700

    progress on badge generation. pdfs look kinda OK-ish, but are missing flags and scannables...

--- src/artifacts/pdfs/badge.py
+++ src/artifacts/pdfs/badge.py
@@ -34,6 +34,12 @@ class BadgeRenderer:
   # badge1 left:  4-1/4
   # ticket0 bottom: 9
   # ticket1 bottom: 10-1/2
+  #
+  # or taken from the bottom of the page:
+  # ticket1 bottom: 0.5"
+  # ticket0 bottom: 2"
+  # badge bottom: 3.5"
+  # badge top: 9.5"
   # 
   # badge shows:
   #   - background image (different versions depending on title/role)
@@ -59,6 +65,11 @@ class BadgeRenderer:
     # Initialize the canvas and layout the badge
     self.layout()
     self.main_box.draw()
+
+    # draw the copy so the badge is double-sided
+    self.main_box.x += 4.25*inch
+    self.main_box.draw()
+
     self.canvas.showPage()
     self.canvas.save()
     return self
@@ -66,33 +77,29 @@ class BadgeRenderer:
   def layout(self):
     # the main box covers the left half of a page and includes the badge, tearaway tickets, etc.
     self.canvas = canvas.Canvas(self.path, pagesize=(8.5*inch, 11*inch))
-    self.main_box = InsetBox(0, 0, 4.25*inch, 11.0*inch, canvas=self.canvas)
+    self.main_box = InsetBox(0, 0.5*inch, 4.25*inch, 9.0*inch, canvas=self.canvas)
     self.margin_size = 0.125*inch
 
-    print(f"Main box origin: ({self.main_box.print_coords()[0]}, {self.main_box.print_coords()[1]})")
-    print(f"Main box dimensions: {self.main_box.width} x {self.main_box.height}")
-
     # the badge area is the region of the page that is perforated to be torn off and stuffed in the badge holder
-    self.badge_box = self.main_box.inset(0, 1.5*inch, 4.25*inch, 6.0*inch)
-    self.badge_box.add_leaf_rounded_rect(colors.white, colors.black, 0.125 * inch, 4.0)
+    self.badge_box = self.main_box.inset(0, 3.0*inch, 4.25*inch, 6.0*inch)
+    self.badge_box.add_leaf_rounded_rect(colors.white, colors.black, 1.0/16.0 * inch, 4.0)
     self.layout_background()
 
-    # next_y = self.layout_logo(self.margin_size).bottom()
-    # next_y = self.layout_info_section(next_y+self.margin_size).bottom()
-    # next_y = self.layout_title_section(next_y+self.margin_size).bottom()
-    # next_y = self.layout_title_section(next_y+self.margin_size).bottom()
-
+    next_y = self.badge_box.height - self.margin_size
+    next_y -= self.layout_logo(next_y).height + self.margin_size
+    next_y -= self.layout_info_section(next_y).height + self.margin_size
+    next_y -= self.layout_title_section(next_y).height + self.margin_size
+    
     return self
   
   def layout_background(self):
-    self.main_box \
-        .inset(0, 0, self.badge_box.width, self.badge_box.height) \
+    self.badge_box \
         .add_leaf_image_centered("src/static/badge_art/2024-player.png")
 
   def layout_logo(self, y):
     logo_height = 1.0 * inch
     logo_width  = 5/4 * logo_height
-    logo_enclosure = self.main_box.inset(0, y, logo_width, logo_height)
+    logo_enclosure = self.badge_box.inset(0, y - logo_height, self.badge_box.width, logo_height)
     logo_enclosure.add_leaf_image_centered("src/static/logos/2025-congress-logo.png")
     return logo_enclosure
   
@@ -102,19 +109,19 @@ class BadgeRenderer:
 
     city_state = "%s, %s" % (info["city"], info["state"])
 
-    info_enclosure = self.main_box.inset(0.5*inch, y, self.main_box.width, info_height)
+    info_enclosure = self.badge_box.inset(0.5*inch, y - info_height, self.badge_box.width-1.0*inch, info_height)
     info_enclosure.add_leaf_rounded_rect(colors.white, colors.gray, 0.05, 4.0)
-    info_enclosure.add_leaf_text_centered(info["name_given"], style(36, colors.black, bold=True), y=0.5*inch)
-    info_enclosure.add_leaf_text_centered(info["name_family"], style(28, colors.black, bold=True), y=1.0*inch)
-    info_enclosure.add_leaf_text_centered(str(self.attendee.id()), style(24, colors.red, bold=True), y=1.75*inch)
-    info_enclosure.add_leaf_text_centered(city_state, style(20, colors.black), y=2.35*inch)
-    info_enclosure.add_leaf_text_centered(self.attendee.badge_rating(), style(48, colors.black, bold=True), y=3.75*inch)
+    info_enclosure.add_leaf_text_centered(info["name_given"], style(36, colors.black, bold=True), y=3.5*inch)
+    info_enclosure.add_leaf_text_centered(info["name_family"], style(28, colors.black, bold=True), y=3.0*inch)
+    info_enclosure.add_leaf_text_centered("#" + str(self.attendee.id()), style(24, colors.red, bold=True), y=2.25*inch)
+    info_enclosure.add_leaf_text_centered(city_state, style(20, colors.black), y=1.65*inch)
+    info_enclosure.add_leaf_text_centered(self.attendee.badge_rating(), style(48, colors.black, bold=True), y=0.25*inch)
 
     return info_enclosure
   
   def layout_title_section(self, y):
     title_height = 0.5*inch
-    title_enclosure = self.main_box.inset(0.5*inch, y, self.main_box.width, title_height)
+    title_enclosure = self.badge_box.inset(0.5*inch, y - title_height, self.badge_box.width - 1.0*inch, title_height)
     title_enclosure.add_leaf_rounded_rect(colors.white, colors.gray, 0.05, 4.0)
     title_enclosure.add_leaf_text_centered(self.attendee.title(), style(28, colors.black, bold=True), y=0.125*inch) # TODO: bold
 
@@ -160,15 +167,13 @@ class InsetBox:
   def absolute_coords(self):
     x = y = 0
     box = self
+
     while box is not None:
       x += box.x
       y += box.y
       box = box.parent
+    
     return [x, y]
-  
-  def print_coords(self, width=0, height=0):
-    x, y = self.absolute_coords()
-    return [x + width, 11.0*inch - y + height] # TODO: read the page height from self.canvas
 
   def add_leaf_rounded_rect(self, fill_color, stroke_color, stroke_width, corner_radius):
     inset = self.inset(0, 0, self.width, self.height)
@@ -178,11 +183,11 @@ class InsetBox:
       canvas.setStrokeColor(stroke_color)
       canvas.setLineWidth(stroke_width)
 
-      print(f"Rectangle origin: ({inset.print_coords()[0]}, {inset.print_coords()[1]})")
+      print(f"Rectangle origin: ({inset.absolute_coords()[0]}, {inset.absolute_coords()[1]})")
       print(f"Rectangle dimensions: {inset.width} x {inset.height}")
 
       canvas.roundRect(
-          *inset.print_coords(),
+          *inset.absolute_coords(),
           inset.width,
           inset.height,
           corner_radius,
@@ -223,7 +228,7 @@ class InsetBox:
       
       canvas.setFont(style.fontName, effective_size)
       canvas.setFillColor(style.fillColor if hasattr(style, "fillColor") else colors.black)
-      canvas.drawString(x - offset_factor*text_width, y, text)
+      canvas.drawString(self.absolute_coords()[0] + x - offset_factor*text_width, self.absolute_coords()[1] + y, text)
 
     inset.draw_func = lambda canvas: draw_the_text(canvas)
 
@@ -242,17 +247,20 @@ class InsetBox:
     
     if img_height > self.height:
       # yes; better fit to height then
-      img_width = self.height * img_aspect_ratio
       img_height = self.height
+      img_width = self.height / img_aspect_ratio
 
+    # Center the image horizontally and vertically within the InsetBox
     img_x = 0.5 * (self.width - img_width)
     img_y = 0.5 * (self.height - img_height)
 
     inset = self.inset(img_x, img_y, img_width, img_height)
     
     def draw_the_image(canvas):
-      img_bottom, img_left = inset.print_coords()
-      canvas.drawImage(img_reader, img_bottom, img_left, width=img_width, height=img_height, mask='auto')
+      img_left, img_bottom = inset.absolute_coords()
+      print(f"Image {image_path}: @({img_left}, {img_bottom}) / ({img_x}, {img_y}), {img_width}x{img_height}")
+      print(f"\tEnclosure: @({self.left()}, {self.bottom()}), {self.width}x{self.height}")
+      canvas.drawImage(img_reader, img_left, img_bottom, width=img_width, height=img_height, mask='auto')
 
     inset.draw_func = lambda canvas: draw_the_image(canvas)
     
@@ -264,7 +272,16 @@ class InsetBox:
     return new_inset
   
   def bottom(self):
-    return self.print_coords()[1]
+    return self.absolute_coords()[1]
+  
+  def top(self):
+    return self.absolute_coords()[1] + self.height
+  
+  def left(self):
+    return self.absolute_coords()[0]
+  
+  def right(self):
+    return self.absolute_coords()[0] + self.width
 
 def style(font_size, color="black", bold=False):
   from reportlab.lib.styles import getSampleStyleSheet
